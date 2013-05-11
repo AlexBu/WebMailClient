b02565d59c88dd8ef1861b846374bb5fb5b18463
Alex Bu <bkpfx000@gmail.com>
2013-05-11 21:25:08 +0800

    update database

--- Clean.bat
+++ Clean.bat
@@ -1,2 +1,4 @@
 copy /y .\WebMailClient\webmaildb.mdb .\WebMailClient\bin\Debug\DB\
+copy /y .\WebMailClient\webmaildb.mdb .\WebMailClient\bin\Release\DB\
 rd /s /q .\WebMailClient\bin\Debug\Mail\
+rd /s /q .\WebMailClient\bin\Release\Mail\
